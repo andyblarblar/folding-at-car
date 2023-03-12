fdf5afa5f7ed5c4d47b025b045c71ab18a9e5189
salmart <92895364+salmart@users.noreply.github.com>
2023-03-11 23:01:10 -0500

    If parked and charging spawn folding at ho

---
+++ car.py
@@ -0,0 +1,11 @@
+from .fordclient import FordClient
+
+Myfile = open("fordclient.py")
+
+class Vehicle():
+   def __init__(self):
+    self.VId #string 
+    self.Charging_Status
+    self.Model
+
+mylist = []
