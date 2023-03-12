8e14c361ae33d11132ea8de335bdbb5a4de396dd
salmart <92895364+salmart@users.noreply.github.com>
2023-03-11 23:07:54 -0500

    If parked and charging at the UofM fleet lab foldcompute, elif foldcompute and not parked at the certain destination stop folding

--- car.py
+++ car.py
@@ -1,11 +1,22 @@
 from .fordclient import FordClient
+from .structs import Location
+import subprocess
+import time
 
-Myfile = open("fordclient.py")
+if __name__== "fordclient":
+    isRunningFold = False
+    mylist = {"8a73d4e286cd6daf0186cd6eac160000", "8a73d4e286cd6daf0186cd6f30230001" }
 
-class Vehicle():
-   def __init__(self):
-    self.VId #string 
-    self.Charging_Status
-    self.Model
+    while(1):
+        for i in mylist:
+           if (FordClient(mylist[i]).getVehicle()[4] == -83.2100190  and FordClient(mylist[i]).getVehicle()[5]==42.3153565 and FordClient(mylist[i]).getVehicle()[2] ):
+               subprocess.run("/bin/bash","star-fah.bash")
+               isRunningFold = True
+           elif (isRunningFold and not(FordClient(mylist[i]).getVehicle()[4] == -83.2100190  and FordClient(mylist[i]).getVehicle()[5]==42.3153565 and FordClient(mylist[i]).getVehicle()[2])) :
+                subprocess.run("/bin/bash","stop-fah.bash")
 
-mylist = []
+
+        time.sleep(1)
+    
+
+
